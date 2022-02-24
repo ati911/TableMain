a9ffca86a311a7656bcfeb4e7cb089f74953a3eb
ati911 <32431778+ati911@users.noreply.github.com>
2022-02-24 18:06:04 +0100

    TablePrint and FilePrintMain class

---
+++ src/main/java/Table/FilePrintMain.java
@@ -0,0 +1,35 @@
+package Table;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import static Table.textDecorations.Decor.WHITE;
+
+public class FilePrintMain {
+    private static final String pathString = "src/main/resources";
+
+    public static void main(String[] args) {
+
+
+
+        Table table = new Table("#", "Fájlnév", "Sorok");
+        table.setHeaderTextColor(WHITE);
+        File[] files = new File(pathString).listFiles();
+        for (int i = 0; i < Objects.requireNonNull(files).length; i++) {
+            String actualFileName = files[i].getName();
+            try {
+                Path pathOfFile = Path.of(pathString + "/" + actualFileName);
+                long linesCounter = Files.lines(pathOfFile).count();
+                table.addRow("" + (i + 1), actualFileName, "" + (linesCounter - 1));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        table.setColumnIsLeftAligned(false, true, false);
+        table.printTable("Elérhető fájlok");
+
+    }
+}
